ac4e395ddd137a4c8c6d72608fd12c6ebd7d067f
MohammoD2 <ibr500037@gmail.com>
2025-12-19 08:41:19 +0600

    adding chatbot api for website

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 if not API_KEY:
-    raise RuntimeError("OPENROUTER_API_KEY environment variable is not set!")
+    raise RuntimeError("OPENROUTER_API_KEY environment variable is not set! ")
 
 API_URL = "https://openrouter.ai/api/v1/chat/completions"
 
